d6eab8c51483d33e3163568305c9acf5907f1731
unknown <tmcfall.prsvr@gmail.com>
2024-02-07 12:28:49 -0500

    2push

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
 // Setup 'tick' sound
 const tick = new Audio('sounds/tick.mp3');
-const tock = new Audio('sound/tock.mp3')
+const tock = new Audio('sounds/tock.mp3')
+const snareDrum = new Audio('sounds/snare-drum.mp3')
+const kickDrum = new Audio('sounds/kick-drum.mp3')
+const hiHat = new Audio('sounds/hi-hat.mp3')
 let count = 0
+
+const kickDrumBox = document.querySelector('#kick-drum')
+const snareDrumBox = document.querySelector('#snare-drum')
+const hiHatBox = document.querySelector('#hi-hat')
+const tickTockBox = document.querySelector('#tickTock')
 
 // This function is called every 600ms
 function update() {
